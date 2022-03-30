3c9888e1816e51406785e38ba4aedf508da28c08
GustavoCastaneda <102257322+GustavoCastaneda@users.noreply.github.com>
2022-03-30 11:56:06 -0600

    Add files via upload

---
+++ truffle-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1:7545",
+      port: 7545,
+      network_id: "*" // Match any network id
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  }
+}
