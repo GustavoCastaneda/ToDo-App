e88291a2f10f03fbfe2c3a9b3cf457a3a28dedf4
GustavoCastaneda <102257322+GustavoCastaneda@users.noreply.github.com>
2022-03-31 13:32:52 -0600

    Update TodoList.sol

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,26 +1,42 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.5.0; 
+pragma solidity ^0.5.0;
 
+contract TodoList {
+  uint public taskCount = 0;
 
- contract TodoList{
-     uint public TaskCount = 0;
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
 
-     struct Task{
-         uint id;
-         string content; 
-         bool completed; 
-     }
+  mapping(uint => Task) public tasks;
 
-    mapping(uint => Task) public tasks;
-        
-    //Esta funcion es para desplegar una tarea por default
-    constructor() public { 
-        createTask("Lavate tus dientes");
-    }
+  event TaskCreated(
+    uint id,
+    string content,
+    bool completed
+  );
 
-    function createTask(string memory _content) public{
-        TaskCount ++;
-        tasks[TaskCount] = Task(TaskCount, _content, false);
-    
-    }     
- }
+  event TaskCompleted(
+    uint id,
+    bool completed
+  );
+
+  constructor() public {
+    createTask("Hola Mundo");
+  }
+
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+    emit TaskCreated(taskCount, _content, false);
+  }
+
+  function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
+  }
+
+}
